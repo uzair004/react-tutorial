067f2dcdbece9465221b43a7192a27c8100acc77
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-04-03 23:25:11 +0500

    app component added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./components/App";
-import App from "./components/Api";
+import App from "./components/App";
+// import App from "./components/Api";
 import "./index.css";
 
 ReactDOM.render(
